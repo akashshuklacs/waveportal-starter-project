574cb0d7f86bb6b0637b18bfe3a1fd62b062bf32
akashshuklacs <akashshuklacse@gmail.com>
2022-03-30 00:20:43 +0530

    connect to ethereum wallet

--- src/App.js
+++ src/App.js
@@ -1,29 +1,82 @@
-import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-export default function App() {
+const App = () => {/*
+    * Just a state variable we use to store our user's public wallet.
+    */
+    const [currentAccount, setCurrentAccount] = useState("");
 
-  const wave = () => {
-    
-  }
-  
-  return (
-    <div className="mainContainer">
+    const checkIfWalletIsConnected = async () => {
+        try {
+        const { ethereum } = window;
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
+        if (!ethereum) {
+            console.log("Make sure you have metamask!");
+            return;
+        }
+        else {
+            console.log("We have ethereum object", ethereum);
+        }
+        const accounts = await ethereum.request({ method : "eth_accounts"});
+
+        if (accounts.lentth !==0) {
+            const account = accounts[0];
+            console.log("Found an authorised account:", account);
+            setCurrentAccount(account);
+        }
+        else {
+            console.log("No authorized account found");
+        }
+        } catch(error) {
+        console.log(error);
+        }
+    }
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                alert("Get MetaMask!");
+                return;
+              }
+
+            const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+
+            console.log("Connected", accounts[0]);
+            setCurrentAccount(accounts[0]);
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, [])
+
+    return (
+        <div className="mainContainer">
+            <div className="dataContainer">
+                <div className="header">
+                👋 Hey there!
+                </div>
+
+                <div className="bio">
+                    I am Akash and I am a software developer!
+                </div>
+
+                <button className="waveButton" onClick={null}>
+                    Wave at Me
+                </button>
+
+            {!currentAccount && (
+                <button className="waveButton" onClick={connectWallet}>
+                    Connect Wallet
+            </button>
+            )}
+            </div>
         </div>
-
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
-      </div>
-    </div>
-  );
-}
+        );
+    }
+export default App
